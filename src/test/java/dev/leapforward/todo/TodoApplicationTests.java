9b657e06ba9d11f28c4fb2933d4c20086af5cb83
Grigoris Iliopoulos <grigoris.iliopoulos@metis.tech>
2023-07-01 20:44:00 +0300

    Use testcontainers for testing

--- src/test/java/dev/leapforward/todo/TodoApplicationTests.java
+++ src/test/java/dev/leapforward/todo/TodoApplicationTests.java
@@ -3,10 +3,29 @@ package dev.leapforward.todo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.ApplicationContext;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class TodoApplicationTests {
+
+	@Container
+	@ServiceConnection
+	private static PostgreSQLContainer postgresqlContainer = new PostgreSQLContainer("postgres:latest")
+			.withDatabaseName("foo")
+			.withUsername("foo")
+			.withPassword("secret");
+
+/*	@DynamicPropertySource
+	private static void setupProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgresqlContainer::getJdbcUrl);
+		registry.add("spring.datasource.username", postgresqlContainer::getUsername);
+		registry.add("spring.datasource.password", postgresqlContainer::getPassword);
+	}*/
 	@Test
 	void contextLoads(ApplicationContext context) {
 		Assertions.assertNotNull(context);
